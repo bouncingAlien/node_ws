cf12ab07d1eb47246606d228b51ca5efd7e290a0
bouncingAlien <dejan.bl@gmail.com>
2018-03-23 21:43:02 +0100

    Created view for items

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
     res.render('index');
 };
@@ -6,6 +9,15 @@ exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' });
 }
 
-exports.createStore = (req, res) => {
-    res.json(req.body);
+exports.createStore = async(req, res) => {
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+    console.log('It worked!');
+    res.redirect(`/store/${store.slug}`);
+}
+
+exports.getStores = async(req, res) => {
+    const stores = await Store.find();
+    console.log(stores);
+    res.render('stores', { title: 'Stores', stores });
 }
