0f31ca9b0d5e8f2a02cf8bb68ae37aaf5896d562
bouncingAlien <dejan.bl@gmail.com>
2018-06-05 17:27:23 +0200

    Created handler for creating and sending email for password reset; Created pug file for sending html email;

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const promisify = require('es6-promisify');
+const mail = require('../handlers/mail');
 
 exports.login = (req, res, next) => {
     passport.authenticate('local', {
@@ -41,8 +42,14 @@ exports.forgot = async(req, res) => {
     user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
     await user.save();
     // 3. send email with token
-    const resetURL = `http//${req.headers.host}/account/reset/${user.resetPasswordToken}`;
-    req.flash('success', `You have been emailed a password reset link. ${resetURL}`);
+    const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+    await mail.send({
+        user,
+        subject: 'Password Reset.',
+        resetURL,
+        filename: 'password-reset'
+    });
+    req.flash('success', `You have been emailed a password reset link.`);
     // 4. redirect to login page 
     res.redirect('/login');
 }
